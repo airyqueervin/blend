d1b562567cae6c4d4bae5d5ce7f767cba181de86
Airyque Ervin <airyqueervin@Airyque-Ervins-MacBook-Air.local>
2018-04-04 12:43:42 -0700

    Made the colors more dynamic.

--- src/components/ColorSwatch.jsx
+++ src/components/ColorSwatch.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
+const colors = ['red', 'green', 'orange', 'blue'];
+
 const ColorSwatch = props => (
   <div id="Colors" sytle={{textAlign: 'center'}}>
-    <div onClick={() => { props.selectColor('red'); props.handleColorChange()}} id="square" style={{backgroundColor: 'red', border: props.color === 'red' ? '2px solid black' : null}} ></div>
-    <div onClick={() => { props.selectColor('green'); props.handleColorChange()}} id="square" style={{backgroundColor: 'green', border: props.color === 'green' ? '2px solid black' : null}} ></div>
-    <div onClick={() => { props.selectColor('orange'); props.handleColorChange()}} id="square" style={{backgroundColor: 'orange', border: props.color === 'orange' ? '2px solid black' : null}} ></div>
-    <div onClick={() => { props.selectColor('blue'); props.handleColorChange()}} id="square" style={{backgroundColor: 'blue', border: props.color === 'blue' ? '2px solid black' : null}} ></div>
+    {colors.map(color => <div onClick={() => { props.selectColor(color); props.handleColorChange()}} id="square" style={{backgroundColor: color, border: props.color === color ? '2px solid black' : null}} ></div> )}
   </div>
 );
 
